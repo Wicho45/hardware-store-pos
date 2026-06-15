364f2d8111915a36fddcd8b9d84c74219b06a551
Wicho45 <126707566+Wicho45@users.noreply.github.com>
2026-06-15 00:20:11 -0600

    [ADD] Ingreso de productos nuevos
    Formulario para ingresar productos y validaciones

--- src/Vista/ingresoProducto.java
+++ src/Vista/ingresoProducto.java
@@ -1,11 +1,13 @@
 package Vista;
 
+import javax.swing.JOptionPane;
+import Modelo.Producto;
+
 public class ingresoProducto extends javax.swing.JFrame {
     
     private static final java.util.logging.Logger logger = java.util.logging.Logger.getLogger(ingresoProducto.class.getName());
 
     public ingresoProducto() {
-        initComponents();
         initComponents();
         this.setTitle("Ingreso de producto - Ferretería Marroquín");
         this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
@@ -13,6 +15,35 @@ public class ingresoProducto extends javax.swing.JFrame {
         this.setLocationRelativeTo(null);
         this.setVisible(true);
     }
+    
+    public boolean verificarContenido(String tipo, String marca, String unidad, String cantidad, String precio, String nivel){
+    
+        if (cantidad.isEmpty() || marca.isEmpty() || nivel.isEmpty() || precio.isEmpty() || tipo.isEmpty() || unidad.isEmpty()){
+            JOptionPane.showMessageDialog(this,"Debe llenar todos los campos", "Error", JOptionPane.ERROR_MESSAGE);
+            return false;
+        }
+        
+        try{
+            
+            Integer.parseInt(cantidad);
+            Integer.parseInt(nivel);
+            
+        }catch(NumberFormatException e){
+            JOptionPane.showMessageDialog(this,"El campo de cantidad y cantidad para advertencia deben ser numeros exactos sin decimales", "Error", JOptionPane.ERROR_MESSAGE);
+            return false;
+        }
+        
+        try{
+            
+            Double.parseDouble(precio);
+            
+        }catch(NumberFormatException e){
+            JOptionPane.showMessageDialog(this,"El campo de precio debe contener decimales. Ejemplo 20.00", "Error", JOptionPane.ERROR_MESSAGE);
+            return false;
+        }
+  
+        return true;
+    };
 
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
@@ -23,7 +54,6 @@ public class ingresoProducto extends javax.swing.JFrame {
         tipoTxt = new javax.swing.JTextField();
         jLabel3 = new javax.swing.JLabel();
         marcaTxt = new javax.swing.JTextField();
-        tipoTxt1 = new javax.swing.JTextField();
         jLabel4 = new javax.swing.JLabel();
         jLabel5 = new javax.swing.JLabel();
         unidadTxt = new javax.swing.JTextField();
@@ -34,8 +64,8 @@ public class ingresoProducto extends javax.swing.JFrame {
         jLabel8 = new javax.swing.JLabel();
         precioTxt = new javax.swing.JTextField();
         jLabel9 = new javax.swing.JLabel();
-        jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
+        cancelarBtn = new javax.swing.JButton();
+        ingresarBtn = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -51,7 +81,6 @@ public class ingresoProducto extends javax.swing.JFrame {
         jLabel3.setText("Tipo de producto:");
         jPanel1.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 60, -1, 20));
         jPanel1.add(marcaTxt, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 140, 390, -1));
-        jPanel1.add(tipoTxt1, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 80, 390, -1));
 
         jLabel4.setFont(new java.awt.Font("Helvetica Neue", 1, 14)); // NOI18N
         jLabel4.setText("Tipo de producto:");
@@ -81,12 +110,13 @@ public class ingresoProducto extends javax.swing.JFrame {
         jLabel9.setText("Precio por unidad:");
         jPanel1.add(jLabel9, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 300, -1, 20));
 
-        jButton1.setText("CANCELAR");
-        jPanel1.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 450, -1, -1));
+        cancelarBtn.setText("CANCELAR");
+        cancelarBtn.addActionListener(this::cancelarBtnActionPerformed);
+        jPanel1.add(cancelarBtn, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 450, -1, -1));
 
-        jButton2.setText("INGRESAR PRODUCTO");
-        jButton2.addActionListener(this::jButton2ActionPerformed);
-        jPanel1.add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(200, 450, -1, -1));
+        ingresarBtn.setText("INGRESAR PRODUCTO");
+        ingresarBtn.addActionListener(this::ingresarBtnActionPerformed);
+        jPanel1.add(ingresarBtn, new org.netbeans.lib.awtextra.AbsoluteConstraints(200, 450, -1, -1));
 
         jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/fondo.png"))); // NOI18N
         jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 450, 500));
@@ -105,14 +135,59 @@ public class ingresoProducto extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jButton2ActionPerformed
+    private void ingresarBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ingresarBtnActionPerformed
+        try{
+            
+        String tipo = tipoTxt.getText().trim().toLowerCase();
+        String marca = marcaTxt.getText().trim().toLowerCase();
+        String unidad = unidadTxt.getText().trim().toLowerCase();
+        String cantidad = cantidadTxt.getText().trim();
+        String precio = precioTxt.getText().trim();
+        String nivel = nivelTxt.getText().trim();
+        
+        if(!verificarContenido(tipo, marca, unidad, cantidad, precio, nivel)){
+            return;
+        }
+        
+        Producto nuevoProducto = new Producto();
+        
+        nuevoProducto.setStock(Integer.parseInt(cantidad));
+        nuevoProducto.setMarca(marca);
+        nuevoProducto.setNivelReorden(Integer.parseInt(nivel));
+        nuevoProducto.setPrecio(Double.parseDouble(precio));
+        nuevoProducto.setTipo(tipo);
+        nuevoProducto.setUnidad(unidad);
+        
+        //debug de prueba
+            System.out.println("Producto nuevo creado con nombre: " + nuevoProducto.getTipo() + " " + nuevoProducto.getMarca());
+        
+        //Logica para ingresar a base de datos SQLite como objeto producto
+        
+        cantidadTxt.setText("");
+        marcaTxt.setText("");
+        nivelTxt.setText("");
+        precioTxt.setText("");
+        tipoTxt.setText("");
+        unidadTxt.setText("");
+        
+        JOptionPane.showMessageDialog(this, "Éxito al registrar el producto", "Éxito", JOptionPane.OK_OPTION);
+            
+        }catch (Exception e){
+            JOptionPane.showMessageDialog(this, "Error al intentar registrar el producto", "Error", JOptionPane.ERROR_MESSAGE);
+            return;
+        }
+        
+        
+    }//GEN-LAST:event_ingresarBtnActionPerformed
+
+    private void cancelarBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cancelarBtnActionPerformed
+        this.dispose();
+    }//GEN-LAST:event_cancelarBtnActionPerformed
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton cancelarBtn;
     private javax.swing.JTextField cantidadTxt;
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
+    private javax.swing.JButton ingresarBtn;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
@@ -127,7 +202,6 @@ public class ingresoProducto extends javax.swing.JFrame {
     private javax.swing.JTextField nivelTxt;
     private javax.swing.JTextField precioTxt;
     private javax.swing.JTextField tipoTxt;
-    private javax.swing.JTextField tipoTxt1;
     private javax.swing.JTextField unidadTxt;
     // End of variables declaration//GEN-END:variables
 }
